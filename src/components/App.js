b5b3479894f2ab1c49d2628b30111f8559ff472c
Cheri Portman <cheriportman@hotmail.com>
2019-05-15 14:29:59 -0700

    brought more images into app

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,19 @@ import PhotoAlbum from './photos/PhotoAlbum';
 
 import threeLoves from '../assets/three-loves.jpg';
 import stitch from '../assets/stitch-on-blue.jpg';
+import mystery1 from '../assets/mystery1.jpg';
+import pb from '../assets/peanut-butter-dipped.jpg';
+import fanno from '../assets/fanno-creek-fun.jpg';
+import dragon from '../assets/dragon-hoard.jpg';
 
 export default function App() {
   const photos = [
     threeLoves,
-    stitch
+    stitch,
+    mystery1,
+    pb,
+    fanno,
+    dragon
   ];
 
   return (
