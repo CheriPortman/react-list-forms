97a2ccb4e99d53c251e1daa0f450f296df7dec40
Cheri Portman <cheriportman@hotmail.com>
2019-05-16 08:28:52 -0700

    created ErrorBoundry.js

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+//display error if an error, console log it
+//otherwise render children componenets
+
+export default class ErrorBoundry extends Component {
+  static propTypes = {
+    children: PropTypes.node
+  }
+
+  state = {
+    error: false
+  }
+
+  static getDerivedStateFromError() {
+    return { error: true };
+  }
+
+  componenetDidCatch(error) {
+    console.error(error);
+  }
+
+  render() { 
+    if(this.state.error) {
+      return <h3>Error! Error!</h3>;
+    }
+
+    return this.props.children;
+  }
+}
