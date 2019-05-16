8491844871959bc253a9c43a4084a89aa019fb14
Cheri Portman <cheriportman@hotmail.com>
2019-05-16 08:32:25 -0700

    Bad.js created

---
+++ src/components/Bad.js
@@ -0,0 +1,6 @@
+//eslint-disable-next-line
+import React from 'react';
+
+export default function Bad() {
+  throw 'nope!';
+}
--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 //display error if an error, console log it
